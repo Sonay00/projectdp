86c3bfdeda85a2ce8b3b3220d21dd28b629d70b5
Sonay00 <ul0280942@edu.uni.lodz.pl>
2024-11-28 21:41:09 +0100

    fix port issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
